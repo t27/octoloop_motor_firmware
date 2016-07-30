2db6f2f1ac69dee29dc4288f5c253cf4d1096201
Kaustubh Nawade <Kaustubh Nawade>
2016-07-31 03:17:03 +0530

    added PID

---
+++ PID/pid.h
@@ -0,0 +1,40 @@
+/*
+ * pid.h
+ *
+ *  Created on: Jul 31, 2016
+ *      Author: Kaustubh
+ */
+
+#ifndef PID_PID_H_
+#define PID_PID_H_
+
+class PID
+{
+    public:
+        // Kp -  proportional gain
+        // Ki -  Integral gain
+        // Kd -  derivative gain
+        // dt -  loop interval time
+        // max - maximum value of manipulated variable
+        // min - minimum value of manipulated variable
+        PID( double dt, double max, double min, double Kp, double Kd, double Ki );
+
+        // Returns the manipulated variable given a setpoint and current process value
+        double calculate( double setpoint, double pv );
+        ~PID();
+
+    private:
+        double _dt;
+        double _max;
+        double _min;
+        double _Kp;
+        double _Kd;
+        double _Ki;
+        double _pre_error;
+        double _integral;
+};
+
+
+
+
+#endif /* PID_PID_H_ */
